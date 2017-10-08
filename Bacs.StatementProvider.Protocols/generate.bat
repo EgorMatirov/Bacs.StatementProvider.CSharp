a466e6640238a2b67a2ac4cadf3a7290affbbc1c
Mikhail Ivchenko <ematirov@gmail.com>
2017-10-08 13:22:24 +0400

    Use Bacs.Protocols.StatementProvider

--- Bacs.StatementProvider.Protocols/generate.bat
+++
@@ -1,8 +0,0 @@
-cd /d %~dp0 
-python cs-protogen\generate.py ^
-    --protoc="../packages/Grpc.Tools/tools/windows_x64/protoc.exe" ^
-    --grpc_csharp_plugin="../packages/Grpc.Tools/tools/windows_x64/grpc_csharp_plugin.exe" ^
-    --output="GeneratedProtos" ^
-    --targets=protos/google_protobuf ^
-    --targets=protos/problem ^
-	--targets=protos/statement_provider
